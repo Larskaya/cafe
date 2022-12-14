c419b3afe84e2151a0484a1d7f696b0b81a7b8a7
larskaya <Llarskaya@yandex.ru>
2022-12-14 23:59:11 +0300

    added styles

--- routs/main_page.py
+++ routs/main_page.py
@@ -4,5 +4,4 @@ from __main__ import app
 
 @app.route('/', methods=['GET'])
 def get():
-    context = {'word': 'cat'}
-    return render_template('main_page.html', context=context)
+    return render_template('main_page.html')
